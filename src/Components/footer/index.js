df6c7a7acbf36c77ff84fd1f3af4a54d51d3b5e1
Desmond <desmond@bluewaterads.com>
2018-10-04 10:04:38 -0700

    updates

--- src/Components/footer/index.js
+++ src/Components/footer/index.js
@@ -17,21 +17,23 @@ class Footer extends Component {
             <div style= {{width: '100%',  backgroundColor: '#2F313D', position: 'relative',  marginTop:'300px'}} >
     <div className="" style={{ height: '1px', position: 'relative', zIndex: '2'}}>
    </div>
+   <div style={{marginLeft:'28%', marginRight:'auto', width:'50%', textAlign: 'center'}}> 
    <Row style={{margin:"10px 0 0 0"}}>
-   <Col md={2}></Col>
-   <Col md={2}> <Link to ="/" className="menu" >Terms & Conditions</Link></Col>
+   <Col md={1}></Col>
+   <Col md={3}> <Link to ="/" className="menu" >Terms & Conditions</Link></Col>
    <Col md={2}>  <Link to ="/contact" className="menu" >Contact</Link></Col>
    <Col md={2}> <Link to ="/about" className="menu" >About Us</Link></Col>
    <Col md={2}><Link to ="/career" className="menu" >Careers</Link></Col>
        <Col md={2}> </Col>
 
    </Row>
+   </div>
               <footer style={{width: '100%', backgroundColor: '#2F313D', position: 'relative', zIndex: '2'}}>
                   <div style={{height: '50px', width: '100%', borderTop: '1px solid #414354', backgroundColor: '#2F313D'}}>
                     <span style={{float: 'left', paddingTop: '10px', marginLeft:'10px'}}>
-                      <img src={require('../../assets/favicon.png')}  alt="heart icon" />
+                      <img src={require('../../assets/favicon.svg')}  alt="heart icon" />
                     </span>
-                    <span className="copyright" style={{marginLeft:'35%', marginRight:'auto'}}>Rating.Ng <p className="copyright">Copyright © 2018 </p><span style={{color: 'transparent'}}> </span></span>
+                    <span className="copyright" style={{marginLeft:'40%', marginRight:'auto'}}>Rating.Ng <p className="copyright">Copyright © 2018 </p><span style={{color: 'transparent'}}> </span></span>
                     <span style={{ float: 'right', paddingTop: '10px', paddingRight: '20px', color: '#6E7493' }}>
                     <i aria-hidden='true' className='facebook f icon socialIcon_default' />
                     </span>
